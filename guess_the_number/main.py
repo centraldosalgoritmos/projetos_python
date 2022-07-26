d335088121683dbe83fc8be443e8cb4bfe8dba0c
Marcos Lima <marcos.lima@icomp.ufam.edu.br>
2022-07-26 17:54:00 -0400

    refact(model-view): separate view and game logic

--- guess_the_number/main.py
+++ guess_the_number/main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from view import Jogo
+from view import JogoGUI
 
 if __name__ == '__main__':
-   jogo = Jogo('JOGO ADVINHE O NÚMERO', '1.0')
-   jogo.iniciar()
+   jogo = JogoGUI('JOGO DO NÚMERO SECRETO', '1.0')
+   jogo.mainloop()
--- guess_the_number/view.py
+++ guess_the_number/view.py
@@ -1,21 +1,16 @@
 # -*- coding: utf-8 -*-
 import tkinter as tk
-
-from random import randint
 from tkinter import messagebox
 
+from model import Jogo
 
-class Jogo:
 
-    __TENTATIVAS_MAX = 5
+class JogoGUI:
 
     def __init__(self, titulo, versao):
         self.titulo = titulo
         self.versao = versao
-        self.numero_secreto = None
-        self.palpite = None
-        self.tentativas = None
-        self.ultimos_palpites = []
+        self.jogo = None
 
         self.root = tk.Tk()
         # self.root.option_add('*Dialog.msg.font', 'arial 32')
@@ -26,7 +21,7 @@ class Jogo:
 
         self.frm_iniciar = tk.Frame(master=self.root, relief=tk.FLAT, borderwidth=3, padx=5, pady=5)
         self.frm_iniciar.pack(fill=tk.X, side=tk.TOP)
-        self.botao_iniciar = tk.Button(master=self.frm_iniciar, text='INICIAR O JOGO', command=self.novo_jogo)
+        self.botao_iniciar = tk.Button(master=self.frm_iniciar, text='INICIAR O JOGO', command=self.action_novo_jogo)
         self.botao_iniciar.pack(fill=tk.X)
 
         self.frm_palpite = tk.Frame(master=self.root, relief=tk.GROOVE, borderwidth=7, padx=5, pady=5)
@@ -39,7 +34,7 @@ class Jogo:
         self.entrada_palpite['state'] = 'disabled'
         self.entrada_palpite.pack(fill=tk.Y, side=tk.LEFT)
 
-        self.botao_palpitar = tk.Button(master=self.frm_palpite, text='PALPITAR', command=self.palpitar)
+        self.botao_palpitar = tk.Button(master=self.frm_palpite, text='PALPITAR', command=self.action_palpitar)
         self.botao_palpitar['state'] = 'disabled'
         self.botao_palpitar.pack(fill=tk.Y, side=tk.RIGHT)
 
@@ -49,45 +44,41 @@ class Jogo:
         self.lbl_ult_palpite = tk.Label(master=self.frm_ultimas, text='ÚLTIMOS PALPITES: ', anchor='w')
         self.lbl_ult_palpite.pack(fill=tk.X, side=tk.BOTTOM)
 
-    def iniciar(self):
+    def mainloop(self):
         self.root.mainloop()
 
-    def novo_jogo(self):
-        self.numero_secreto = randint(1, 100)
-        self.tentativas = Jogo.__TENTATIVAS_MAX
+    def action_novo_jogo(self):
+        self.jogo = Jogo(1, 100, 7)
         self.botao_palpitar['state'] = 'normal'
         self.botao_iniciar['state'] = 'disabled'
         self.entrada_palpite['state'] = 'normal'
         self.entrada_palpite.delete(0, 'end')
-        self.lbl_ult_palpite.configure(text='ÚLTIMOS PALPITES: ')
-        messagebox.showinfo(title='BOM JOGO!', message=f'O SISTEMA ESCOLHEU UM NÚMERO SECRETO ENTRE 1 E 100!\nVOCÊ TEM {self.tentativas} PARA ACERTÁ-LO.')
+        self.entrada_palpite.focus()
+        messagebox.showinfo(
+            title='BOM JOGO!',
+            message=f'O SISTEMA ESCOLHEU UM NÚMERO SECRETO ENTRE {self.jogo.get_n_min()} E {self.jogo.get_n_max()}!\nVOCÊ TEM {self.jogo.get_n_tentativas()} TENTAVIAS PARA ACERTÁ-LO.'
+        )
 
-    def palpitar(self):
+    def action_palpitar(self):
         palpite = int(self.string_palpite.get())
-        self.ultimos_palpites.append(str(palpite))
-        self.lbl_ult_palpite.configure(text=f"ÚLTIMOS PALPITES {','.join(self.ultimos_palpites)}")
-        if palpite == self.numero_secreto:
-            self.numero_secreto = None
-            self.tentativas = None
-            self.palpite = None
-            self.botao_palpitar['state'] = 'disabled'
-            self.botao_iniciar['state'] = 'normal'
-            self.entrada_palpite['state'] = 'disabled'
+
+        if self.jogo.palpitar(palpite):
             messagebox.showinfo(title='PARABÉNS!', message='VOCÊ ACERTOU O NÚMERO SECRETO!')
         else:
-            self.tentativas -= 1
-            if palpite > self.numero_secreto:
-                messagebox.showinfo(title='ERROOOOU!', message=f'SEU PALPITE FOI MAIOR!\nRESTAM {self.tentativas} TENTATIVAS')
-            else:
-                messagebox.showinfo(title='ERROOOOU!', message=f'SEU PALPITE FOI MENOR!\nRESTAM {self.tentativas} TENTATIVAS')
+            messagebox.showinfo(title='ERROOOOU!', message=f'{self.jogo.get_dica()}\nRESTAM {self.jogo.get_n_tentativas()} TENTATIVAS.')
+            self.lbl_ult_palpite.configure(text=f'ÚLTIMOS PALPITES: {self.jogo.get_palpites()}')
+            self.entrada_palpite.delete(0, 'end')
+            self.entrada_palpite.focus()
 
-            if self.tentativas < 1:
-                messagebox.showinfo(title='FIM DE JOGO!', message=f'NÃO RESTAM MAIS TENTATIVAS!\nO NÚMERO SECRETO ERA {self.numero_secreto}.')
-                self.numero_secreto = None
-                self.tentativas = None
-                self.palpite = None
-                self.ultimos_palpites = []
-                self.botao_palpitar['state'] = 'disabled'
-                self.botao_iniciar['state'] = 'normal'
-                self.entrada_palpite['state'] = 'disabled'
-                self.entrada_palpite.delete(0, 'end')
+        if self.jogo.is_fim_jogo():
+            self.action_fim_jogo()
+
+    def action_fim_jogo(self):
+        messagebox.showinfo(title='FIM DE JOGO!', message=f'NÃO RESTAM MAIS TENTATIVAS!')
+        self.jogo = None
+        self.entrada_palpite.delete(0, 'end')
+        self.botao_iniciar['state'] = 'normal'
+        self.botao_palpitar['state'] = 'disabled'
+        self.entrada_palpite['state'] = 'disabled'
+        self.entrada_palpite.delete(0, 'end')
+        self.lbl_ult_palpite.configure(text='ÚLTIMOS PALPITES: ')
